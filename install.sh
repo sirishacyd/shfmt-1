eba2725ea4c006c59c52316e9d0b721912e1d46b
sirisha polisetty <sirishacyd@gmail.com>
2022-05-18 13:14:24 -0700

    change 3

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#
+# This is change 3
 # This script should be run via curl:
 #   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # or via wget:
